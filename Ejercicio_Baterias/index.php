ab24c6c617571d7fbc73cb12b86dc76613266a74
Adrian-DWES <agolteanu01@educarex.es>
2021-10-27 12:39:40 +0200

    commit 27 octubre

---
+++ Ejercicio_Baterias/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baterías</title>
+</head>
+
+<body>
+    Código: <input type="text" name="codigo" id="codigo" placeholder="Código de las baterías"><br>
+    Tipo: <select name="tipo" id="tipo">
+        <option value="plomo" selected>Plomo-ácido (PB-ácido)</option>
+        <option value="niquel">Níquel-hidruro metálico (NiMh)</option>
+        <option value="litio">Ion-litio (LiCoO2)</option>
+        <option value="catodo">Ion-litio con cátodo de LiFePO4</option>
+        <option value="polimero">Polímero de litio (LiPo)</option>
+    </select><br>
+    Marca: <input type="text" name="marca" id="marca" placeholder="Marca del coche"><br>
+    Modelo: <input type="text" name="modelo" id="modelo" placeholder="Modelo del coche"><br>
+    Capacidad: <input type="text" name="carga" id="carga" placeholder="Carga de las baterías(%)"><br>
+    Ciclos de vida: <input type="text" name="ciclos" id="ciclos" placeholder="Ciclos de vida restantes"><br><br>
+    <table>
+        <tr>Est </tr>
+        <tr>Estacion </tr>
+        <tr>Fecha </tr>
+        <tr>Matricula</tr>
+    </table>
+</body>
+
+</html>
