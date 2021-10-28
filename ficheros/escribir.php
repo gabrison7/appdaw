5f67c789b87ae937f19ff7200cb42530e5f5ae89
Adrian-DWES <agolteanu01@educarex.es>
2021-10-28 13:31:59 +0200

    commit 28 octubre 13:31

---
+++ ficheros/escribir.php
@@ -0,0 +1,18 @@
+<?php
+
+ //Abrir el archivo
+ $archivo = 'tablas.txt';
+ $abrir = fopen($archivo, 'a+');ación de los productos
+
+ // Descripción precio stock
+ $producto1 = 'Lavadora Industrial-1500-20'.chr(13).chr(10);
+ $producto2 = 'Televisión 40 pulgadas-3500-30'.chr(13).chr(10);
+ $producto3 = 'Refrigeradora 12 pies - 5500 - 10';
+
+//Enviar información de los productos
+ fputs($abrir, $producto1);
+ fputs($abrir, $producto2);
+ fputs($abrir, $producto3);
+
+//Cerrar el archivo
+ fclose($abrir);
