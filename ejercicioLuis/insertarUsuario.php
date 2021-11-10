a2b13caf1739e1ceba86aded42d5cae82a19e2fa
Adrian-DWES <agolteanu01@educarex.es>
2021-11-10 12:21:35 +0100

    commit 12:21

---
+++ ejercicioLuis/insertarUsuario.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once 'conexion.php';
+$config= parse_ini_file('datos.ini');
+$conexionPDO =  obtenerConexionPDO($config);
+// Comprobar la conexión
+try{
+
+    //CONSTRUIMOS LA SENTENCIA SQL
+    $sql = "INSERT INTO usuario (nombre, sexo, edad) VALUES ('htm', 23, 'luismr@ies.es')";
+    // se usa exec() porque la sentencia no devuelve ningún valor
+    $conexionPDO->exec($sql);
+    echo "<p>Nueva fila creada correctamente";
+ 
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+
+  $conexionPDO = null;
+    
+    
+    ?>
